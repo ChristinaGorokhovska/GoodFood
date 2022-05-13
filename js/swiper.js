7d6cb8c8cbc9767f3f02eb6dcec68346fbe3a296
ChristinaGorokhovska <kristinagorokhovska@gmail.com>
2022-05-13 21:31:40 +0300

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+});
